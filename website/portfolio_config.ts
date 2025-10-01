59e15a585b39ff554738b51f931e550d3d87190b
Moaz Mirza <moazmirza13@gmail.com>
2025-10-02 03:05:45 +0500

    test: vercel server fn

--- website/portfolio_config.ts
+++ website/portfolio_config.ts
@@ -2,7 +2,7 @@ const API_URL = import.meta.env.VITE_API_URL;
 const GITHUB_USER = import.meta.env.VITE_GITHUB_USERNAME;
 
 if (!API_URL || !GITHUB_USER) {
-  throw new Error("❌ Missing GEMINI_API_KEY or GITHUB_USERNAME in .env file.");
+  throw new Error("❌ Missing API_URL or GITHUB_USERNAME in .env file.");
 }
 
 const MODEL_NAME = "gemini-2.5-flash-lite";
