48bb7903b9639ba54ad69357fb2f0bae352330be
Moaz Mirza <moazmirza13@gmail.com>
2025-11-13 20:48:16 +0500

    feat: vercel analytics

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import { ChatbotWidget } from "dev-portfolio-chatbot";
 import { portfolio_config } from "../portfolio_config";
 import { useChatbotSettings } from "./hooks/useChatbotSettings";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   const { size, setSize, position, setPosition, theme, applyTheme } =
@@ -10,6 +11,8 @@ function App() {
 
   return (
     <>
+      <Analytics />
+
       <Home
         size={size}
         setSize={setSize}
